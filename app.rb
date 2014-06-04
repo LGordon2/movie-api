d3e691e6a16f1393658cea2fdb8220a9cc5b7202
lew <LGordon2@gmail.com>
2014-06-04 13:29:12 -0400

    Added callback functionality

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require ::File.expand_path('../models/movie',__FILE__)
 #config
 configure do
   enable :sessions
-  
+
   set :haml, layout_options: {views: 'views/layouts'}
   set :sessions, :domain => 'orasi.com'
 end
@@ -15,7 +15,9 @@ end
 get '/' do
   unless params[:search].nil?
     @movie = Movie.find_by(title: params[:search].split(" ").collect {|w| w.capitalize}.join(" "))
-    jbuilder :index unless @movie.nil?
+    unless @movie.nil?
+      params[:callback].nil? ? (jbuilder :index) : "#{params[:callback]}(#{jbuilder :index});"
+    end
   else
     haml :index, :layout => :main
   end
