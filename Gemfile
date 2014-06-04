dcca1c4c9d58ae762ab9ebff23d381dec576a59b
lew <LGordon2@gmail.com>
2014-06-04 13:16:20 -0400

    Added default views

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ gem 'sinatra'
 gem "tilt-jbuilder", ">= 0.4.0", :require => "sinatra/jbuilder"
 gem 'sqlite3'
 gem 'activerecord', require: 'active_record'
+gem 'haml'
--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
-# myapp.rb
+# app.rb
 require ::File.expand_path('../config/application',__FILE__)
+
 Bundler.require(:default)
 require ::File.expand_path('../models/movie',__FILE__)
+
+#config
+configure do
+  set :haml, layout_options: {views: 'views/layouts'}
+end
 
 get '/' do
   unless params[:search].nil?
     @movie = Movie.find_by(title: params[:search].split(" ").collect {|w| w.capitalize}.join(" "))
     jbuilder :index unless @movie.nil?
   else
-    erb :index
+    haml :index, :layout => :main
   end
 end
